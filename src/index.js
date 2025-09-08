5f348df96e7e351b49275707d0bd23bf1dfb7260
yashazguardtech <yash.t@azguardstech.com>
2025-09-08 19:34:17 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-/*import './index.css';
-import App from './App';*/
-import StarRaiting from './Star';
+import './index.css';
+import App from './App';
+/*import StarRaiting from './Star';*/
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <StarRaiting maxwidth={10}/>
+    <App/>
   </React.StrictMode>
 );
 
