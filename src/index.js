dcefa64ae2719f390d53702fb3c4d8cdcbfc632c
yashazguardtech <yash.t@azguardstech.com>
2025-09-15 15:34:39 +0530

    App v-1 add

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+/*import App from './App';*/
 /*import StarRaiting from './Star';*/
+import App from './App-v1'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
